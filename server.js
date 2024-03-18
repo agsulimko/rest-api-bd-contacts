ab947b6773ac3682f08b5690904ef3345ea42bc4
Artem <a.g.sulimko@gmail.com>
2024-03-18 23:12:19 +0100

    add

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+// const DB_HOST =
+//   "mongodb+srv:Artem:5Gu6JbN56BqOvwg6@cluster0.b1qvw6h.mongodb.net/contact-book?retryWrites=true&w=majority";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Server running. Use our API on port: 3000");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
