1ec321e36bcf0867ae88ba62b9473b623cf26b8f
Artem <a.g.sulimko@gmail.com>
2024-03-19 20:39:00 +0100

    add

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
-
 // const contactsRouter = require("./routes/api/contacts");
-
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -13,7 +11,6 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
 // app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const app = require("./app");
+require("dotenv").config();
 const DB_HOST =
   "mongodb+srv://Artem:5Gu6JbN56BqOvwg6@cluster0.b1qvw6h.mongodb.net/contact-book?retryWrites=true&w=majority";
 
-const { PORT = 4000 } = process.env;
+// const { DB_HOST, PORT = 4000 } = process.env;
 
+const PORT = 4000;
 mongoose.set("strictQuery", true);
 
 mongoose
